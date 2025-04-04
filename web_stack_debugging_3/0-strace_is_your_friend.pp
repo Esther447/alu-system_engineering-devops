e0c81fd8d6ec00157c1e7332c1203f7ced2ed378
Esther447 <e.mushimiyi@alustudent.com>
2025-04-04 20:58:40 +0200

    0-strace_is_your_friend.pp

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,14 @@
 # run sed command with puppet to fix the mistake of file name
 # class-wp-locale.php instead of class-wp-locale.phpp in wp-settings.php
-# Fixes WordPress file permissions to resolve Apache 500 Internal Server Error
-exec { 'fix-wordpress':
+# Fixes WordPress file permissions and configures Apache to resolve 500 Internal Server Error
+exec { 'fix-wordpress-permissions':
   command => 'chown -R www-data:www-data /var/www/html',
   path    => ['/bin', '/usr/bin'],
+  notify  => Service['apache2'],
+}
+
+service { 'apache2':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
 }
