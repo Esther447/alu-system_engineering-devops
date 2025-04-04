4b0b73e26f7cde2e9d6dc8ce0bb71849d13d95a5
Esther447 <e.mushimiyi@alustudent.com>
2025-04-05 00:27:10 +0200

    0-strace_is_your_friend.pp

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,7 +2,7 @@
 # class-wp-locale.php instead of class-wp-locale.phpp in wp-settings.php
 # Fixes WordPress file permissions and configures Apache to resolve 500 Internal Server Error
 exec { 'fix-wordpress':
-  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+  command => 'bash -c "sudo sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
 /var/www/html/wp-settings.php; service apache2 restart"',
   path    => '/usr/bin:/usr/sbin:/bin'
 }
